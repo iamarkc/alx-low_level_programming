95af02f575611132b73311e5bd5f53acd13d3f31
iamarkc <iamarkmaina@gmail.com>
2023-08-14 11:05:56 -0700

    Macros_are_fun

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LIKE_MACRO_3
+#define _FUNCTION_LIKE_MACRO_3
+
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+#endif /* _FUNCTION_LIKE_MACRO_3 */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_4
+#define _SUM_4
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* _SUM_4 */
